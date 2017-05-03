d823b7fbdf9d8176a98c02782dacf8b6b1ea1476
Alexander Babai <aliaksandr.babai@gmail.com>
2017-05-03 23:17:32 +0300

    rafactor(views): rename vars

--- src/components/Views/RevokeToken/RevokeToken.jsx
+++ src/components/Views/RevokeToken/RevokeToken.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-export default class Auth extends Component {
+export default class RevokeToken extends Component {
     render() {
         return (
             <div>
--- src/components/Views/Settings/Settings.jsx
+++ src/components/Views/Settings/Settings.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-export default class SettingsPage extends Component {
+export default class Settings extends Component {
     render() {
         return (
             <div>
