88f2781e5a1b5bb5689392c3e971aa97dcb7a229
Alexander Babai <aliaksandr.babai@gmail.com>
2017-05-03 23:15:28 +0300

    feat(header): new component bring up

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Nav, Navbar, NavItem } from 'react-bootstrap'
+
+const Header = () => (
+    <Navbar collapseOnSelect fluid>
+        <Navbar.Header>
+            <Navbar.Brand>
+                <span>React-Bootstrap</span>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+            <Nav>
+                <NavItem eventKey={1} href="/">Home</NavItem>
+                <NavItem eventKey={2} href="/settings">Settings</NavItem>
+            </Nav>
+            <Nav pullRight>
+                <NavItem eventKey={3} href="/revoke-token">Revoke Token</NavItem>
+            </Nav>
+        </Navbar.Collapse>
+    </Navbar>
+)
+
+export default Header
